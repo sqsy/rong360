fc80e30ea2ab24e0d7fdf21eb069fba0a0f39fc1
soffolk <zlx.star@gmail.com>
2014-12-03 21:38:55 +0800

    use UITableView show html text

--- ios/app/controllers/show_controller.rb
+++ ios/app/controllers/show_controller.rb
@@ -1,10 +1,5 @@
-class ShowController < UIViewController
+class ShowController < UITableViewController
   BASE_URL = "http://www.1bd1.com/forum.php"
-
-  def loadView
-   self.view = UIWebView.alloc.init
-   self.view.delegate = self
-  end
 
   def initWithPost(post)
     @post = post
@@ -15,13 +10,47 @@ class ShowController < UIViewController
   def viewDidLoad
     super
 
-    url = "#{BASE_URL}?mod=viewthread&tid=#{@post.id}&extra=page%3D1&mobile=2"
-    request = NSURLRequest.requestWithURL(NSURL.URLWithString(url))
-    view.loadRequest(request)
+    self.navigationItem.title = @post.title
+
+    #self.tableView.rowHeight = 100
+    @comments = []
+    @post.seek do |comments|
+      @comments = comments
+      self.tableView.reloadData
+    end
   end
 
-  def webViewDidFinishLoad(webView)
-    p webView.stringByEvaluatingJavaScriptFromString("window.location.href")
+  def tableView(tableView, cellForRowAtIndexPath: indexPath)
+    @reuseIdentifier ||= "DETAIL_CELL_IDENTIFIER"
+
+    cell = tableView.dequeueReusableCellWithIdentifier(@reuseIdentifier) || begin
+      UITableViewCell.alloc.initWithStyle(UITableViewCellStyleDefault, reuseIdentifier:@reuseIdentifier)
+    end
+
+    p @comments[indexPath.row].message
+
+    attributed_string = @comments[indexPath.row].message.attributed_html
+    textView = UITextView.alloc.init
+    textView.AttributedText = attributed_string
+    rect = attributed_string.boundingRectWithSize(CGSizeMake(cell.frame.size.width, 10000), options:NSStringDrawingUsesLineFragmentOrigin | NSStringDrawingUsesFontLeading, context:nil)
+    textView.frame = rect
+    textView.scrollEnabled = false
+    textView.editable = false
+
+    cell.contentView.addSubview(textView)
+
+    cell
+  end
+
+  def tableView(tableView, numberOfRowsInSection: section)
+    @comments.count
+  end
+
+  def tableView(tableView, heightForRowAtIndexPath: indexPath)
+    cell = self.tableView(tableView, cellForRowAtIndexPath: indexPath)
+    attributed_string = @comments[indexPath.row].message.attributed_html
+    rect = attributed_string.boundingRectWithSize(CGSizeMake(cell.frame.size.width, 10000), options:NSStringDrawingUsesLineFragmentOrigin | NSStringDrawingUsesFontLeading, context:nil)
+    return rect.size.height
   end
 
 end
